2d70015fd9924e143d7226253a656881d97d96b6
arzymat01 <arzymatarkabaev01@gmail.com>
2023-09-11 22:16:02 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/agro_koom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agro_koom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/image.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Image.asset('assets/images/icon.png'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
