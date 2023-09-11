b714e82d4f15f78ad414d8bc83436f0c80699e44
arzymat01 <arzymatarkabaev01@gmail.com>
2023-09-11 23:08:20 +0600

    HomeView

---
+++ lib/constants/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color green = Color(0xff00BE00);
+  static const Color white = Color(0xffFFFFFF);
+}
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:agro_koom/constants/color.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -23,9 +24,54 @@ class _HomeViewState extends State<HomeView> {
           ),
           Column(
             children: [
-              Image.asset('assets/images/icon.png'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(150, 130, 12, 10),
+                child: Image.asset('assets/images/icon.png'),
+              ),
             ],
-          )
+          ),
+          const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'AGROKOOM',
+                  style:
+                      TextStyle(fontSize: 45, color: Appcolors.white, shadows: [
+                    Shadow(
+                      offset: Offset(-1.5, -1.5),
+                      color: Appcolors.green,
+                    )
+                  ]),
+                ),
+                Text(
+                  'Фермер менен ишкерди ортомчусуз \n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tбайланыштыруу',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Appcolors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(160, 50, 55, 80),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Войти',
+                    style: TextStyle(
+                      color: Appcolors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
